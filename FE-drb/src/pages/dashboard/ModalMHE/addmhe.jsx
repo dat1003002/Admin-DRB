32fa8766bca8d22dc487904e1044c96a2919c69c
unknown <phanvandat10032002@gmail.com>
2024-07-25 16:54:57 +0700

    file

--- FE-drb/src/pages/dashboard/ModalMHE/addmhe.jsx
+++ FE-drb/src/pages/dashboard/ModalMHE/addmhe.jsx
@@ -104,7 +104,7 @@ const AddMhe = ({ open, handleOpen }) => {
                             </div>
                             <div className="">
                                 <div className="flex justify-between">
-                                    <label className="text-black font-medium text-base">Độ dày(≠ 0.3mm):</label>
+                                    <label className="text-black font-medium text-base">Độ dày(± 0.3mm):</label>
                                     <input type="text" name="doday" value={formData.doday} onChange={handleChange} className="border border-black ml-3 w-[300px] text-black pl-1" />
                                 </div>
                                 <div className="mt-4 border border-black pb-4">
@@ -134,11 +134,11 @@ const AddMhe = ({ open, handleOpen }) => {
                                     </div>
                                 </div>
                                 <div className="flex justify-between mt-4">
-                                    <label className="text-black font-medium text-base">Tốc độ máy đùn(≠50):</label>
+                                    <label className="text-black font-medium text-base">Tốc độ máy đùn(±50):</label>
                                     <input type="text" name="tocdomaydun" value={formData.tocdomaydun} onChange={handleChange} className="border border-black ml-1 w-[300px] text-black pl-1" />
                                 </div>
                                 <div className="flex justify-between mt-4">
-                                    <label className="text-black font-medium text-base">Tốc độ Kéo(≠50):</label>
+                                    <label className="text-black font-medium text-base">Tốc độ Kéo(±50):</label>
                                     <input type="text" name="tocdokeo" value={formData.tocdokeo} onChange={handleChange} className="border border-black ml-1 w-[300px] text-black pl-1" />
                                 </div>
                             </div>
